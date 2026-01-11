ce84739e7305271fc27c71a3465d54ae77ee43ad
6cbjjjwdxm-lgtm <6cbjjjwdxm@privaterelay.appleid.com>
2026-01-11 20:17:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const token = '8476471291:AAEISZPCvzK4GjefGB9jtUf6yiCPZqq98zI';
 const bot = new TelegramBot(token, {webHook: true});
 
 const app = express();
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 
-app.post('/webhook', (
+app.post('/webhook', (req, res) => {
+  res.status(200).send('OK'); 
+  bot.processUpdate(req.body); 
+});
+
+const port = process.env.PORT || 10000;
+app.listen(port, () => {
+  console
