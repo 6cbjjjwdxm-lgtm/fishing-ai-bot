10ad8e1b788b9a941938ea6ab21fb06099a631d8
6cbjjjwdxm-lgtm <6cbjjjwdxm@privaterelay.appleid.com>
2026-01-11 19:24:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 require('dotenv').config();
-console.log('dotenv загружен');
-
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.TELEGRAM_TOKEN;
-console.log('Токен:', token ? 'OK' : 'ПУСТОЙ!');
-
-if (!token) {
-  console.log('ОШИБКА: TELEGRAM_TOKEN отсутствует');
-  process.exit(1);
-}
-
+const token = '8476471291:AAEISZPCvzK4GjefGB9jtUf6yiCPZqq98zI'; 
 const bot = new TelegramBot(token, {webHook: true});
 
 const app = express();
 app.use(express.json());
 
-app.post('/webhook', (req
+app.post('/webhook', (
