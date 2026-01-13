ff7c5c46894fc20ff099b1de61627263265137b6
6cbjjjwdxm-lgtm <6cbjjjwdxm@privaterelay.appleid.com>
2026-01-13 11:48:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,2 @@
 require('dotenv').config();
-const express = require('express');
-const TelegramBot = require('node-telegram-bot-api');
-
-const token = '8476471291:AAEISZPCvzK4GjefGB9jtUf6yiCPZqq98zI'; 
-const bot = new TelegramBot(token, {webHook: true});
-
-const app = express();
-app.use(express.json({limit: '10mb'}));
-
-app.post('/webhook', (req, res) => {
-  res.status(200).send('OK'); 
-  bot.processUpdate(req.body); 
-});
-
-const port = process.env.PORT || 10000;
-app.listen(port, () => {
-  console
+const express = require('
